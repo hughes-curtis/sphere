149c69778b4167f124c52caa5f175598e2bad1b5
unknown <hughescurtis99@gmail.com>
2023-08-26 21:40:50 +0300

    fixed email send

--- src/app.js
+++ src/app.js
@@ -13,12 +13,20 @@ const hiddenElements = document.querySelectorAll('.back');
 hiddenElements.forEach((el) => observer.observe(el));
 
 const contactBtn = document.getElementById('contact-button');
+const contactForm = document.getElementById('contact-form');
+const messageSpan = document.getElementById('sent-message');
 
-contactBtn.addEventListener('click', () => {
-        emailjs.sendForm('default_service', 'template_a864buh', '#contact-form')
+contactBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+        emailjs.sendForm('service_de3271b', 'template_qdgkji2', '#contact-form')
         .then(function(response) {
-        console.log('SUCCESS!', response.status, response.text);
+            console.log('SUCCESS!', response.status, response.text);
         }, function(error) {
         console.log('FAILED...', error);
         });
+    contactForm.style.display = 'none';
+    messageSpan.style.display = 'block';
+
+        
 })
